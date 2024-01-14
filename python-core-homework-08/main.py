fc96840309e2abcb0202cd745badc871c898c28f
Art_m <artem_madrid@hotmail.com>
2024-01-14 14:57:00 +0100

    hw_08

--- python-core-homework-08/main.py
+++ python-core-homework-08/main.py
@@ -3,26 +3,22 @@ from collections import defaultdict
 
 
 def get_birthdays_per_week(users):
-   
+
     birthdays = defaultdict(list)
     current_date = date.today()
     days_of_week = [(current_date + timedelta(days=i)) for i in range(0, 7)]
 
-
     for user in users:
         bd_date = user["birthday"]
         
+        if bd_date < current_date:
+            bd_date = bd_date.replace(year=current_date.year + 1)
+            
         if current_date.weekday() == 0:
-            weekend = [(current_date + timedelta(days=i-2)) for i in range(0, 2)]
-            if bd_date in weekend:
-                birthdays["Monday"].append(user["name"])
-        if current_date.weekday() == 1:
-            weekend_day = [current_date - timedelta(days=2)]
-            if bd_date in weekend_day:
-                birthdays["Monday"].append(user["name"])
+            days_of_week = [(current_date + timedelta(days=i-2)) for i in range(0, 7)]
 
         if bd_date in days_of_week:
-            if bd_date.weekday() == 0:
+            if bd_date.weekday() == 0 or bd_date.weekday() == 5 or bd_date.weekday() == 6:
                 birthdays["Monday"].append(user["name"])
             if bd_date.weekday() == 1:
                 birthdays["Tuesday"].append(user["name"])
@@ -34,15 +30,3 @@ def get_birthdays_per_week(users):
                 birthdays["Friday"].append(user["name"])
 
     return birthdays
-
-
-if __name__ == "__main__":
-    users = [
-        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
-    ]
-
-    result = get_birthdays_per_week(users)
-    print(result)
-    # Виводимо результат
-    for day_name, names in result.items():
-        print(f"{day_name}: {', '.join(names)}")
--- test.py
+++ test.py
@@ -1,26 +1,21 @@
 from datetime import date, datetime, timedelta
 from collections import defaultdict
 
-# .strftime('%Y-%m-%d')
 
 def get_birthdays_per_week(users):
-   
+
     birthdays = defaultdict(list)
-    current_date = date(2023, 12, 30)
+    current_date = date.today()
     days_of_week = [(current_date + timedelta(days=i)) for i in range(0, 7)]
-    print()
 
     for user in users:
         bd_date = user["birthday"]
         
         if current_date.weekday() == 0:
-            weekend = [(current_date + timedelta(days=i-2)) for i in range(0, 2)]
-            print(bd_date in weekend)
-            if bd_date in weekend:
-                birthdays["Monday"].append(user["name"])
+            days_of_week = [(current_date + timedelta(days=i-2)) for i in range(0, 7)]
 
         if bd_date in days_of_week:
-            if bd_date.weekday() == 0:
+            if bd_date.weekday() == 0 or bd_date.weekday() == 5 or bd_date.weekday() == 6:
                 birthdays["Monday"].append(user["name"])
             if bd_date.weekday() == 1:
                 birthdays["Tuesday"].append(user["name"])
@@ -32,9 +27,3 @@ def get_birthdays_per_week(users):
                 birthdays["Friday"].append(user["name"])
 
     return birthdays
-
-
-print(get_birthdays_per_week(users = [
-        {"name": "Jan Koum", "birthday": datetime(2024, 1, 3).date()},
-        {"name": "Peter", "birthday": datetime(2024, 1, 7).date()}
-    ]))
