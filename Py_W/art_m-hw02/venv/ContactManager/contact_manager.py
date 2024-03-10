25c54d180273df42044b0b51d12c82bd43e739fa
Art_m <artem_madrid@hotmail.com>
2024-03-10 20:24:59 +0100

    py_web hw02

---
+++ Py_W/art_m-hw02/venv/ContactManager/contact_manager.py
@@ -0,0 +1,202 @@
+"""Console bot helper"""
+
+from ContactManager.models import ObjectValidateError, AddressBook, Record, Name, Phone
+from tools.common import CommandHandler, handle_error
+from view.base_view import ConsoleView
+
+
+def handle_validation_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ObjectValidateError as e:
+            print(f"Validation error occurred: {e}")
+            return None
+        except IOError as e:
+            print(f"I/O error occurred: {e}")
+            return None
+    return wrapper
+
+
+class ContactManager:
+    def __init__(self, address_book, view):
+        self.address_book = address_book
+        self.view = view
+
+    @handle_error
+    @handle_validation_errors
+    def handle_add_contact(self):
+        name = input('Name: ')
+        phones_input = input('Phones (comma-separated, 10 digits only): ')
+        phones = [phone.strip() for phone in phones_input.split(',')]
+        email = input('Email (optional): ')
+        birthday = input('Birthday (optional, dd.mm.yyyy): ')
+        address = input('Address (optional): ')
+
+        record = Record(name, email=email, birthday=birthday, address=address)
+        record.phones.extend(Phone(phone_number) for phone_number in phones)
+        self.address_book.add_record(record)
+        self.address_book.save_data_to_file()
+        self.view.display_message(f'Contact {name} added.')
+
+    @handle_error
+    @handle_validation_errors
+    def handle_change_contact(self):
+        name_to_change = input('Enter the name of the contact to change: ')
+        contact = self.address_book.find(name_to_change)
+
+        if contact:
+            name = input('Name: ')
+            phones_input = input('Phones (comma-separated, 10 digits only): ')
+            phones = [phone.strip() for phone in phones_input.split(',')]
+            email = input('Email (optional): ')
+            birthday = input('Birthday (optional, dd.mm.yyyy): ')
+            address = input('Address (optional): ')
+
+            contact.name = Name(name)
+            contact.phones = [Phone(phone) for phone in phones]
+            if email:
+                contact.add_email(email)
+            if birthday:
+                contact.set_birthday(birthday)
+            if address:
+                contact.add_address(address)
+            self.address_book.save_data_to_file()
+            self.view.display_message(f'Contact {name_to_change} changed.')
+        else:
+            self.view.display_message(f'Contact with name "{name_to_change}" not found.')
+
+    @handle_error
+    def handle_delete_contact(self):
+        name = self.view.get_input("Enter the name of the contact to delete: ")
+        if self.address_book.find(name):
+            self.address_book.delete(name)
+            self.address_book.save_data_to_file()
+            self.view.display_message(f"Contact '{name}' successfully deleted.")
+        else:
+            self.view.display_message(f"Contact with the name '{name}' not found.")
+
+    @handle_error
+    def handle_get_contact_by_name(self):
+        name = self.view.get_input("Enter the name of the contact to search for: ")
+        record = self.address_book.find(name)
+        if record:
+            self.view.display_message(str(record))
+        else:
+            self.view.display_message(f"Contact with the name '{name}' not found.")
+
+    @handle_error
+    def handle_search_contacts(self):
+        query = self.view.get_input("Enter the query to search for contacts: ")
+        found_contacts = self.address_book.search_full(query)
+        if found_contacts:
+            self.view.display_message("Found contacts:")
+            for contact in found_contacts:
+                self.view.display_message(str(contact))
+        else:
+            self.view.display_message(f"No contacts found for the query '{query}'.")
+
+    def handle_display_all_contacts(self):
+        all_contacts = self.address_book.iterator()
+        found_contacts = False
+        for page in all_contacts:
+            for contact in page:
+                self.view.display_message(str(contact))
+                found_contacts = True
+        if not found_contacts:
+            self.view.display_message("No contacts found.")
+
+    @handle_error
+    def handle_congratulate(self):
+        """
+        Handles the congratulate command.
+
+        Asks the user to input the number of days for congratulations.
+        If a valid number is provided, it congratulates all contacts whose birthdays are within the specified number
+         of days.
+        If no contacts with birthday information are found, it displays a message accordingly.
+
+        Raises:
+            ValueError: If the input provided is not a valid number.
+        """
+        days = self.view.get_input("Enter the number of days for congratulations: ")
+        try:
+            days = int(days)
+            for record in self.address_book.data.values():
+                if record.birthday:
+                    result = record.birthday.congratulate(self.address_book, days)
+                    self.view.display_message(result)
+                    return
+            self.view.display_message("No contacts with birthday information found.")
+        except ValueError:
+            self.view.display_message("Please enter a valid number of days.")
+
+
+class ContactCommandHandler(CommandHandler):
+    def __init__(self, manager, view):
+        super().__init__(manager, view)
+        commands = {
+            '1': ("Add contact", manager.handle_add_contact),
+            '2': ("Change contact", manager.handle_change_contact),
+            '3': ("Delete contact", manager.handle_delete_contact),
+            '4': ("Get contact info", manager.handle_get_contact_by_name),
+            '5': ("Search", manager.handle_search_contacts),
+            '6': ("Show all contacts", manager.handle_display_all_contacts),
+            '7': ("Greets", manager.handle_congratulate),
+            '0': ("Return to main menu", self.return_to_main_menu())
+        }
+        super().__init__(commands, view)
+        self.manager = manager
+
+    def handle_command(self, choice):
+        """
+        Handle the selected command.
+
+        :param choice: User's choice of command.
+        """
+        command = self.commands.get(choice)
+        if command:
+            command[1]()
+        else:
+            self.view.display_message('Invalid choice. Please select a valid option.')
+
+    def return_to_main_menu(self):
+        """
+        Return to the main menu.
+        """
+        return
+
+
+def run_contact_manager():
+    program_name = "Contact Manager V0.1"
+    view = ConsoleView()
+    address_book = AddressBook()
+    address_book.load_data_from_file()
+    manager = ContactManager(address_book, view)
+    contact_command_handler = ContactCommandHandler(manager, view)
+
+    while True:
+        options = {
+            '1': 'Add contact',
+            '2': 'Change contact',
+            '3': 'Delete contact',
+            '4': 'Get contact by name',
+            '5': 'Search contacts',
+            '6': 'Show all contacts',
+            '7': 'Congratulate',
+            '0': 'Return to Main Menu'
+        }
+        choice = view.display_menu(program_name, options)
+        if choice in ['1', '2', '3', '4', '5', '6', '7']:
+            contact_command_handler.handle_command(choice)
+        elif choice == '0':
+            return
+        else:
+            view.display_message('Invalid choice. Please select a valid option.')
+
+
+#    address_book.save_data_to_file()
+
+
+if __name__ == '__main__':
+    run_contact_manager()
---
+++ Py_W/art_m-hw02/venv/FileManager/file_sorter.py
@@ -0,0 +1,137 @@
+import os
+import shutil
+import re
+from view.base_view import ConsoleView
+
+
+# Dictionary mapping file types to their corresponding extensions
+DIRECTORIES = {
+    "Images": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", ".svg", ".heif", ".psd"],
+    "Videos": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng", ".qt", ".mpg", ".mpeg", ".3gp"],
+    "Documents": [".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",
+                  ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
+                  ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt",
+                  ".pptx", ".csv"],
+    "Audio": [".aac", ".aa", ".aacp", ".dsd", ".dvf", ".m4a", ".m4b", ".m4p",
+              ".mp3", ".msv", ".ogg", ".oga", ".raw", ".vox", ".wav", ".wma"],
+    "Text": [".txt", ".in", ".out"],
+    "Programming": [".py", ".ipynb", ".c", ".cpp", ".class", ".h", ".java",
+                    ".sh", ".html", ".css", ".js", ".go", ".json"]
+}
+
+
+def normalize(name):
+    """
+    Normalize file name to replace special characters and spaces with underscores.
+
+    Args:
+        name (str): File name to be normalized.
+
+    Returns:
+        str: Normalized file name.
+    """
+    translit = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+                'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
+                'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+                'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shc', 'ъ': '',
+                'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+    name = name.lower()
+    for cyr, lat in translit.items():
+        name = name.replace(cyr, lat)
+    name = re.sub(r'[^\w\s-]', '_', name)
+    name = re.sub(r'\s+', ' ', name)
+    name = name.strip().replace(' ', '_')
+    return name
+
+
+def create_directories(root):
+    """
+    Create directories for different types of files.
+
+    Args:
+        root (str): Root directory where the directories will be created.
+    """
+    for directory in DIRECTORIES:
+        directory_path = os.path.join(root, directory)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+
+
+def process_file(file_path, root):
+    """
+    Process a file and move it to the corresponding directory based on its extension.
+
+    Args:
+        file_path (str): Path to the file to be processed.
+        root (str): Root directory where the file is located.
+    """
+    for directory, extensions in DIRECTORIES.items():
+        for extension in extensions:
+            if file_path.lower().endswith(extension):
+                file_directory = os.path.join(root, directory)
+                file_name, file_ext = os.path.splitext(os.path.basename(file_path))
+                file_name = normalize(file_name) + file_ext
+                destination = os.path.join(file_directory, file_name)
+                shutil.move(file_path, destination)
+                return
+
+    archive_extensions = [".zip", ".tar", ".gz"]
+    if any(file_path.lower().endswith(extension) for extension in archive_extensions):
+        archive_name, _ = os.path.splitext(os.path.basename(file_path))
+        archive_directory = os.path.join(root, "Archives", archive_name)
+        if not os.path.exists(archive_directory):
+            os.makedirs(archive_directory)
+        shutil.unpack_archive(file_path, archive_directory)
+
+        os.remove(file_path)
+        return
+
+    unknown_directory = os.path.join(root, "Unknown")
+    if not os.path.exists(unknown_directory):
+        os.makedirs(unknown_directory)
+    destination = os.path.join(unknown_directory, os.path.basename(file_path))
+    shutil.move(file_path, destination)
+    return
+
+
+def process_directory(root):
+    """
+    Process all files in a directory and move them to their corresponding directories.
+
+    Args:
+        root (str): Root directory containing the files to be processed.
+    """
+    print("File sorting in progress ...")
+    create_directories(root)
+    for path, _, files in os.walk(root):
+        for file in files:
+            file_path = os.path.join(path, file)
+            process_file(file_path, root)
+    print("File sorting completed.")
+
+
+def delete_empty_directories(root):
+    """
+    Delete empty directories within the root directory.
+
+    Args:
+        root (str): Root directory containing directories to be checked and deleted if empty.
+    """
+    for dirpath, dirnames, filenames in os.walk(root, topdown=False):
+        if not dirnames and not filenames:
+            os.rmdir(dirpath)
+
+
+def run_file_sorter():
+    """
+    Entry point to run the file sorting process.
+    """
+    view = ConsoleView()
+    path = view.get_input("Enter the path to directory you want to sort: ")
+    delete_empty_directories(path)
+    create_directories(path)
+    process_directory(path)
+
+
+if __name__ == '__main__':
+    run_file_sorter()
